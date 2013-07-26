823326b380ffae3bfc42f67b9a2936f299829de6
Darren Muff <darren.muff@mac.com>
2013-07-26 07:32:52 +0100

    Initial Commit

---
+++ GOSS/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  GOSS
+//
+//  Created by Darren on 26/07/2013.
+//  Copyright (c) 2013 Dstl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
